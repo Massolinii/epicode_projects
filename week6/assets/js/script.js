d079f47d87a0fdc36ef69348fc56bd00f63dd35b
Massolinii <massimilianoesposito548@gmail.com>
2023-03-10 16:24:26 +0100

    daily

--- week6/assets/js/script.js
+++ week6/assets/js/script.js
@@ -1,20 +1,16 @@
-const carousel = document.querySelector("#title-carousel .carousel-inner");
-
-// Creo una copia degli elementi originali
-const items = Array.from(carousel.querySelectorAll(".carousel-item")).map((item) => item.cloneNode(true));
-
-// Aggiungo la copia alla fine del carosello
-items.forEach((item) => {
-  carousel.appendChild(item);
+window.addEventListener("scroll", function () {
+  // codice per gestire lo scrolling
 });
 
-// Aggiungo un event listener per la pressione dei bottoni
-document.querySelectorAll("#title-carousel .carousel-control-prev, #title-carousel .carousel-control-next").forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.classList.contains("carousel-control-prev")) {
-      carousel.insertBefore(carousel.lastElementChild, carousel.firstElementChild);
-    } else {
-      carousel.appendChild(carousel.firstElementChild);
-    }
-  });
-});
+const navbar = document.getElementById("navbar");
+const navbarPosition = navbar.offsetTop;
+
+function updateNavbar() {
+  if (window.pageYOffset >= navbarPosition) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+window.addEventListener("scroll", updateNavbar);
