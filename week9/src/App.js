f3b0009149fc5b18b095301670ef16062e707410
Massolinii <massimilianoesposito548@gmail.com>
2023-03-31 15:33:02 +0200

    Functional build, savepoint

--- week9/src/App.js
+++ week9/src/App.js
@@ -3,25 +3,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from './components/Navbar'
 import FilmCarousel from './components/FilmCarousel';
 import Footer from './components/Footer';
+import Jumbotron from './components/Jumbotron';
 
 function App() {
   const potter = "harry%20potter"
   const marvel = "marvel"
   const starWars = "star%20wars"
-  const bond = "james%20bond"
+  const bond = "spy"
   const spongebob = "spongebob"
 
   return (
     <div className="App">
       <NavBar />
-      <br></br>
-      <br></br>
-      <br></br>
+      <Jumbotron />
       <h2 className='collectionTitle'>The Boy Who Lived - Harry Potter's Saga</h2>
       <FilmCarousel myEndpoint={potter}/>
       <h2 className='collectionTitle'>Star Wars - The Original Sixology :</h2>
       <FilmCarousel myEndpoint={starWars}/> 
-      <h2 className='collectionTitle'>James Bond Collection</h2>
+      <h2 className='collectionTitle'>Action - Spy Movies</h2>
       <FilmCarousel myEndpoint={bond}/>
       <h2 className='collectionTitle'>Latest from Marvel Cinematic Universe :</h2>
       <FilmCarousel myEndpoint={marvel}/>
