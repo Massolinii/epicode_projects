36db013038689a124433bf371b9932fa9296d08f
Massolinii <massimilianoesposito548@gmail.com>
2023-04-07 01:06:17 +0200

    Start

---
+++ week10/meteo_app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CustomNavbar from "./components/CustomNavbar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CustomNavbar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
