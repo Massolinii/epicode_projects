6c4338696361403d3a9c148e9a1f7e34688fcffb
Massolinii <massimilianoesposito548@gmail.com>
2023-10-07 12:39:05 +0200

    Removed FavButton

--- week10/meteo_app/src/components/Home.jsx
+++ week10/meteo_app/src/components/Home.jsx
@@ -1,8 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import MainHome from "./MainHome";
-import { Link } from "react-router-dom";
 import Searchbar from "./Searchbar";
-import FavButton from "./FavButton";
 
 const cities = ["Milano", "Roma", "Napoli"];
 
@@ -12,19 +10,10 @@ const MyHome = () => {
       <Row>
         <Searchbar />
       </Row>
-
       <Row>
         {cities.map((city, index) => (
           <MainHome key={index} cityName={city} />
         ))}
-      </Row>
-
-      <Row>
-        <Col className="col-12 w-100 mx-auto">
-          <Link to="/your-cities" id="home-link">
-            <FavButton />
-          </Link>
-        </Col>
       </Row>
     </Container>
   );
